d53f99373199971aeae751dfd8700264e3ab32a1
Martin Flores <martinfloresgrez@gmail.com>
2025-06-05 22:27:25 -0400

    saved UNDO information for reset changes and discarded files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
